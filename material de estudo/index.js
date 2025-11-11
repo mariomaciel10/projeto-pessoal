6110452ec53d615d19a032fc5558a8fde67e5369
Mario Pietro <piedromario7@gmail.com>
2025-11-11 11:51:16 -0300

    Create index.js

---
+++ material de estudo/index.js
@@ -0,0 +1,2 @@
+"Esse projeto visa ajudar os estudantes a se organizar com assuntos do conteudo que caira no vestibular ou na avaliação parcial"
+"Esse projeto no momento sera disponibilizado apenas nessa EEEP 
